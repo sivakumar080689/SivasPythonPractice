b33879a988786234f4ed7a0d7e8b26ff0fbda71d
Sivakumar <sivakumar.bala89@gmail.com>
2024-01-25 07:29:52 -0400

    NP_Day1notes

---
+++ NP_Day1Notes.py
@@ -0,0 +1,34 @@
+print('I work for Amazon')
+
+a='2023-02-01'
+
+print(a)
+print(type(a))
+
+str1 = 'i am siva'
+str2 = 'i live in halifax'
+
+city = str2.split()[-1]
+print(city)
+print(city.upper())
+
+print(city[1:3])
+print(city[:-1])
+print(city[:-1])
+
+str3 = str1+" "+str2+" this is python course"
+print(str3)
+name = 'siva'
+lastname = 'kumar'
+age = 35
+print(f"{name} {lastname} lives in halifax ")
+print(f"{name} age is {age}")
+
+print(1+2)
+company = "amazon"
+print(company*3)
+
+num  = 12
+print(type(num))
+
+print(int(32)+3)
